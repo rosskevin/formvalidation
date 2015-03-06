f0bed891363f010dcb8c88db35f33ec554ba2d9d
nghuuphuoc <phuoc@huuphuoc.me>
2015-03-06 17:34:03 +0700

    Fix ignored behaviour

--- src/js/base.js
+++ src/js/base.js
@@ -1449,13 +1449,9 @@ if (typeof jQuery === 'undefined') {
                         }
 
                         var isValidContainer = this.isValidContainer($parent);
-                        if (isValidContainer !== null) {
-                            $parent.removeClass(this.options.row.valid).removeClass(this.options.row.invalid);
-
+                        if (status !== this.STATUS_IGNORED && isValidContainer !== null) {
                             // Don't add success/error class to the container if the field is ignored
-                            if (status !== this.STATUS_IGNORED) {
-                                $parent.addClass(isValidContainer ? this.options.row.valid : this.options.row.invalid);
-                            }
+                            $parent.removeClass(this.options.row.valid).removeClass(this.options.row.invalid).addClass(isValidContainer ? this.options.row.valid : this.options.row.invalid);
                         }
                         break;
 
